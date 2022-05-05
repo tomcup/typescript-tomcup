ca6bccca75ce4066a38d8beb1a8037a2ec103b53
tomcup <jiligulu7755@outlook.com>
2022-05-05 12:33:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-hw88pj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-hw88pj)
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+import $ = require('jquery');
+
+// Write TypeScript code!
+const appDiv: HTMLElement = $('#app').get(0);
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
